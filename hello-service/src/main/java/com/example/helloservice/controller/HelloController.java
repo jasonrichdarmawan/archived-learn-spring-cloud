8955e4db30e951608d771615160e1c2c96f797b2
Jason <jason.onggo@tempatkerja.com>
2020-12-01 19:20:54 +0700

    add eureka service

--- hello-service/src/main/java/com/example/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/example/helloservice/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.helloservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 
+  @Value("${message}")
+  private String message;
+
   @GetMapping("/hello")
   public ResponseEntity<Object> getHello() {
-    return ResponseEntity.ok().body("Hello");
+    return ResponseEntity.ok().body("Hello" + message);
   }
 }
--- world-service/src/main/java/com/example/worldservice/controller/WorldController.java
+++ world-service/src/main/java/com/example/worldservice/controller/WorldController.java
@@ -1,14 +1,21 @@
 package com.example.worldservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class WorldController {
 
+  @Value("${message}")
+  private String message;
+
+  @RefreshScope
   @GetMapping("/world")
   public ResponseEntity<Object> getWorld() {
-    return ResponseEntity.ok().body("World");
+    return ResponseEntity.ok().body("World" + message);
   }
 }
